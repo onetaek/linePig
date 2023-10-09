7701366f42baacca594156c29f435cf41d23e8f2
won taek oh <dnjsxorchlrh@naver.com>
2023-10-09 18:59:38 +0900

    deploy: cloudtype 관련 배포 설정3
    - 배포방식war -> jar로 변경

--- src/main/java/com/shop/linepig/ServletInitializer.java
+++ src/main/java/com/shop/linepig/ServletInitializer.java
@@ -1,13 +1,9 @@
 package com.shop.linepig;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+public class ServletInitializer {
 
-public class ServletInitializer extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(LinePigApplication.class);
-    }
-
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(LinePigApplication.class);
+//    }
 }
